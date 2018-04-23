a0e05452d97999624dffddc1bf49873418cc3c25
Tina Lee <l.tn3314@gmail.com>
2018-04-23 15:18:12 -0400

    add prompt for user to accept or decline file transfer

--- receive.py
+++ receive.py
@@ -1,3 +1,4 @@
+import os
 import socket
 
 def receiveFile(clientsocket, filename):
@@ -12,3 +13,26 @@ def receiveFile(clientsocket, filename):
         print(clientmsg.decode('ascii'), flush=True)
 
         clientmsg = clientsocket.recv(bufsize)
+
+def prompt_user(clientsocket):
+    userresponse = ''
+    while (userresponse != 'y' and userresponse != 'n'):
+        # TODO: update with filename
+        userresponse = input('Would you like to accept the following file (y/n): %s ' % 'foo.txt')
+
+    if (userresponse == 'n'):
+        # TODO: send declined code to client
+        clientsocket.send('declined'.encode('ascii'))
+    else:
+        newfilepath = 'placeholder'
+        while (newfilepath != ''):
+            try:
+                newfilepath = input('Enter in the path for where you would like to save the file (/path/to/filename.txt): ')
+                os.makedirs(os.path.dirname(newfilepath), exist_ok=True)
+                # TODO: call receiveFile with newfilepath
+                newfile = open(newfilepath, 'w')
+                newfile.close()
+                return
+            except:
+                print('Invalid pathname!', flush=True)
+                continue
