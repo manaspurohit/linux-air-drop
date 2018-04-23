5545d1b2f8f18f8e41e4351df04a34f14e232b5e
Manas Purohit <purohit.m@husky.neu.edu>
2018-04-23 16:33:33 -0400

    added hash checking

--- receive.py
+++ receive.py
@@ -1,6 +1,7 @@
 import os
 import socket
 import struct
+import hashlib
 
 next = "next".encode('ascii')
 bufsize = 4096
@@ -20,15 +21,27 @@ def receiveFile(clientsocket, filepath):
         else:
             datasize = filesize
 
-        clientmsg = clientsocket.recv(datasize)
-        print(clientmsg)
-        f.write(clientmsg)
-        readyForNext(clientsocket)
+        receive_data(clientsocket, f, datasize)
+
         filesize = filesize - datasize
         if filesize == 0:
             break
 
+    print("done")
     f.close()
+
+def receive_data(clientsocket, file, datasize):
+    clientmsg = clientsocket.recv(datasize)
+    readyForNext(clientsocket)
+    hash = clientsocket.recv(bufsize)
+    myhashlib = hashlib.md5()
+    myhashlib.update(clientmsg)
+    if hash != myhashlib.digest():
+        clientsocket.send("hash doesn't match".encode('ascii'))
+        receive_data(clientsocket, file, datasize)
+    else:
+        file.write(clientmsg)
+        readyForNext(clientsocket)
 
 def readyForNext(clientsocket):
     clientsocket.send(next)
@@ -42,21 +55,20 @@ def prompt_user(clientsocket):
     filename = clientmsg.decode('ascii')
     userresponse = ''
     while (userresponse != 'y' and userresponse != 'n'):
-        # TODO: update with filename
         userresponse = input('Would you like to accept the following file (y/n): %s ' % filename)
 
     if (userresponse == 'n'):
-        # TODO: send declined code to client
         clientsocket.send('declined'.encode('ascii'))
     else:
         newfilepath = 'placeholder'
-        while (newfilepath != ''):
+        while True:
             try:
                 newfilepath = input('Enter in the path for where you would like to save the file (/path/to/filename.txt): ')
                 os.makedirs(os.path.dirname(newfilepath), exist_ok=True)
-                readyForNext(clientsocket)
-                receiveFile(clientsocket, newfilepath)
-                return
             except:
                 print('Invalid pathname!', flush=True)
                 continue
+
+            readyForNext(clientsocket)
+            receiveFile(clientsocket, newfilepath)
+            return
